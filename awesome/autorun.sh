2f560a6be1ac2c2ffe39c09fbabc6f2033c9c6b2
Lu Fengqi <curiouslfq@gmail.com>
2026-08-01 22:17:13 +0800

    awesome: update autostart apps

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -6,14 +6,10 @@ function run {
   fi
 }
 
-run "xrandr --output HDMI1 --auto --output DP1 --auto --right-of HDMI1"
 run "picom --config /home/luke/.config/compton.conf"
 run "light-locker"
 run "fcitx5"
-run "pidgin"
 run "nextcloud"
-run "syncthing-gtk"
 run "blueman-applet"
-run "telegram-desktop"
-run "sakura"
-run "chromium"
+run "Telegram"
+run "google-chrome-canary"
